46092d88682164f3a9140c6159d9548173f5b455
boradoriii <syjoo0126@naver.com>
2023-11-09 16:01:59 +0900

    5 수정본

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
+
 	FILE* fp;
 	char str[100];
 	int i;
-	
+
+
 	//1. open file
-	fp = fopen("sample.txt","W");
-	
+	fp = fopen("sample.txt","w");
+
 	//2. write file
 	for (i=0; i<3; i++){
-	
+
 	//2-1. print "input a word"
 	printf("input a word:");
 	//2-2. scanf a string
@@ -25,8 +25,6 @@ int main(int argc, char *argv[]) {
 }
 	//3. close file
 	fclose(fp);
-	
+
 	return 0;
 }
-
-
